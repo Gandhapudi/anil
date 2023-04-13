5f01f397f749cabacd8d67bc33057c1cfb069c60
Gandhapudi <86158321+Gandhapudi@users.noreply.github.com>
2023-04-13 17:32:32 +0530

    Add files via upload

---
+++ code .js
@@ -0,0 +1,29 @@
+
+
+
+function output(){
+    //take input from documnet.getelementbyid value as value of a
+   let a= "9391405720";
+   let sum=0
+   let d;
+    for (let names of a){
+       sum+=Number(names)
+    }
+    for (let i=1;i<sum;i++){
+        if(i%4==0 && i%5==0){
+           d+=" Fizz Buzz"
+        }
+        else if(i%4==0){
+            d+=" Fizz"
+        }
+        else if(i%5==0){
+           d+=" Buzz"
+        }
+        else{
+            d+=" "+i
+        }
+    }
+  
+   console.log(d)
+}
+output()
